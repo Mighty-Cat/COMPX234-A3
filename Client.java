118c883c449dc0671fb7c767c257e3b893912d2e
MightyCat <3300190576@qq.com>
2025-04-27 20:10:45 +0800

    obtain request of client
    The obtainRequest() method of the Client class has been added, which reads the text and converts it into the request format

--- Client.java
+++ Client.java
@@ -1,4 +1,7 @@
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Client extends Thread{
     private Socket socket;
@@ -20,5 +23,39 @@ public class Client extends Thread{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String obtainRequest(String requestFilePath){
+        String request = "";     
+        try (Stream<String> lines = Files.lines(Paths.get(requestFilePath))) {
+            lines.forEach(line -> {
+                line = line.trim();
+                    if (!line.isEmpty()) {
+                        String[] massages = line.split(" ");               
+                        if (massages[0] == "PUT") {
+                            String value = String.join(" ", java.util.Arrays.copyOfRange(massages, 2, massages.length));//Get the value of the request
+                            //Check if the length of the value is less than or equal to 970
+                            if (massages[1].length() + value.length() > 970) {
+                                System.out.printf("Error: %s exceeds the maximum length. Ignoring.%n", line);
+                                return;
+                            }
+                            String lengthCode = String.format("%03d", String.format("000 P %s %s", massages[1], value).length());//Get the length of the request
+                            request = String.format("%s P %s %s",lengthCode, massages[1], value);
+                        } else if (massages[0] == "READ") {
+                            String lengthCode = String.format("%03d", String.format("000 R %s", massages[1]).length());
+                            request = String.format("%s R %s", lengthCode, massages[1]);
+                        } else if (massages[0] == "GET") {
+                            String lengthCode = String.format("%03d", String.format("000 G %s", massages[1]).length());
+                            request = String.format("%s G %s", lengthCode, massages[1]);
+                        } else {
+                            System.out.printf("Error: Invalid command in line: %s%n", line);
+                            return;
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        return request;
     }
 }
